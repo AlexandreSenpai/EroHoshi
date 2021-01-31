28688db908075fd679761038b028c0b592f3be39
Alexandre Senpai <alexandrebsramos@hotmail.com>
2021-01-31 10:46:02 -0300

    .env ignore

--- frontend/src/services/firebase.js
+++ frontend/src/services/firebase.js
@@ -2,13 +2,13 @@ import firebase from 'firebase/app';
 import "firebase/auth";
 
 const app = firebase.initializeApp({
-    apiKey: "AIzaSyBabXHAtG0l-HOs80E20C38fx31nT5C1i4",
-    authDomain: "eroneko.firebaseapp.com",
-    projectId: "eroneko",
-    storageBucket: "eroneko.appspot.com",
-    messagingSenderId: "636848523194",
-    appId: "1:636848523194:web:fc3caa2183a96dffc83f83",
-    measurementId: "G-VCW28C014K"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID
 });
 
 export const auth = app.auth();
